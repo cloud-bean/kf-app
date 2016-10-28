6c4a85112022716af7f2c0d55b9c379bc09e609e
xinnix <xinnix2000@gmail.com>
2016-10-28 11:43:52 +0800

    clear dirty code

--- src/config/util.js
+++ src/config/util.js
@@ -1,6 +1,4 @@
-const request = require('superagent');
 const querystring = require('querystring');
-const localMoment = require('momentjs');
 import config from './config';
 
 function getAuthorizeURL(appid, redirect, state, scope) {
@@ -15,103 +13,103 @@ function getAuthorizeURL(appid, redirect, state, scope) {
 
   return `${url}?${querystring.stringify(info)}#wechat_redirect`;
 }
-
-function auth(appid, code) {
-  return new Promise((resolve, reject) => {
-    request.get(config.route.auth)
-      .query({
-        appid,
-        code,
-      })
-      .then((result) => {
-        console.log('auth', result);
-        const userid = result.body.userid;
-        const accessToken = result.body.accessToken;
-        resolve({
-          userid,
-          accessToken,
-        });
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-function getUserInfo(userid, accessToken) {
-  return new Promise((resolve, reject) => {
-    request.get(`${config.route.base}/users/${userid}`)
-      // .withCredentials()
-      .set('Authorization', `Bearer ${accessToken}`)
-      .then((result) => {
-        resolve(result.body.data);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-function getTaskList(accessToken) {
-  return new Promise((resolve, reject) => {
-    request.get(`${config.route.base}/tasks/`)
-      // .withCredentials()
-      .set('Authorization', `Bearer ${accessToken}`)
-      .then((result) => {
-        resolve(result.body.data);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-function getRanks(accessToken) {
-  return new Promise((resolve, reject) => {
-    request.get(`${config.route.base}/ranks`)
-      // .withCredentials()
-      .set('Authorization', `Bearer ${accessToken}`)
-      .then((result) => {
-        resolve(result.body.data);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-function friendLike(friendId, accessToken) {
-  return new Promise((resolve, reject) => {
-    request.post(`${config.route.base}/upVotes`)
-      .send({ userId: friendId })
-      // .withCredentials()
-      .set('Authorization', `Bearer ${accessToken}`)
-      .then((result) => {
-        resolve(result.body.data);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-
-function getLevel(exp) {
-  const levelExp = [50, 100, 200, 350, 550, 800, 1100, 1450, 1850, 2300, 2800, 3350, 3950, 4600, 5300, 6050, 6850, 7700, 8600, 9550];
-  const maxlevel = levelExp.length;
-  for (const key in levelExp) {
-    if (exp < levelExp[key]) return Number(key) + 1;
-  }
-  return maxlevel;
-}
+//
+// function auth(appid, code) {
+//   return new Promise((resolve, reject) => {
+//     request.get(config.route.auth)
+//       .query({
+//         appid,
+//         code,
+//       })
+//       .then((result) => {
+//         console.log('auth', result);
+//         const userid = result.body.userid;
+//         const accessToken = result.body.accessToken;
+//         resolve({
+//           userid,
+//           accessToken,
+//         });
+//       })
+//       .catch((err) => {
+//         reject(err);
+//       });
+//   });
+// }
+//
+// function getUserInfo(userid, accessToken) {
+//   return new Promise((resolve, reject) => {
+//     request.get(`${config.route.base}/users/${userid}`)
+//       // .withCredentials()
+//       .set('Authorization', `Bearer ${accessToken}`)
+//       .then((result) => {
+//         resolve(result.body.data);
+//       })
+//       .catch((err) => {
+//         reject(err);
+//       });
+//   });
+// }
+//
+// function getTaskList(accessToken) {
+//   return new Promise((resolve, reject) => {
+//     request.get(`${config.route.base}/tasks/`)
+//       // .withCredentials()
+//       .set('Authorization', `Bearer ${accessToken}`)
+//       .then((result) => {
+//         resolve(result.body.data);
+//       })
+//       .catch((err) => {
+//         reject(err);
+//       });
+//   });
+// }
+//
+// function getRanks(accessToken) {
+//   return new Promise((resolve, reject) => {
+//     request.get(`${config.route.base}/ranks`)
+//       // .withCredentials()
+//       .set('Authorization', `Bearer ${accessToken}`)
+//       .then((result) => {
+//         resolve(result.body.data);
+//       })
+//       .catch((err) => {
+//         reject(err);
+//       });
+//   });
+// }
+//
+// function friendLike(friendId, accessToken) {
+//   return new Promise((resolve, reject) => {
+//     request.post(`${config.route.base}/upVotes`)
+//       .send({ userId: friendId })
+//       // .withCredentials()
+//       .set('Authorization', `Bearer ${accessToken}`)
+//       .then((result) => {
+//         resolve(result.body.data);
+//       })
+//       .catch((err) => {
+//         reject(err);
+//       });
+//   });
+// }
+//
+//
+// function getLevel(exp) {
+//   const levelExp = [50, 100, 200, 350, 550, 800, 1100, 1450, 1850, 2300, 2800, 3350, 3950, 4600, 5300, 6050, 6850, 7700, 8600, 9550];
+//   const maxlevel = levelExp.length;
+//   for (const key in levelExp) {
+//     if (exp < levelExp[key]) return Number(key) + 1;
+//   }
+//   return maxlevel;
+// }
 
 export default {
   getAuthorizeURL,
-  auth,
-  getUserInfo,
-  getTaskList,
-  getLevel,
-  getRanks,
-  localMoment,
-  friendLike,
+  // auth,
+  // getUserInfo,
+  // getTaskList,
+  // getLevel,
+  // getRanks,
+  // localMoment,
+  // friendLike,
 };
