c972069b3ced88b2e63da25c0d26ece7f2a85760
xinnix <xinnix2000@gmail.com>
2016-11-07 07:47:12 +0800

    update accesstoken

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ export default {
     testRedirect: 'http://localhost:8080?code=123',
   },
   secret: {
-    accessToken: '17e24e8535d937211ff5ed1e01e783ab50d84d99dce8af1bf0a4d9598c7f1d20',
+    accessToken: '540334efbefc0c2f24765d230f486d79a6dccdf595f18a0845165010874ffc77',
     userid: '5817407d2a2d3adc2032290e',
   },
 };
