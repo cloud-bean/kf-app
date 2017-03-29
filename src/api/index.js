7d58850f790e6ad7afaeaf3ed71b957863c6c689
xinnix <xinnix2000@gmail.com>
2017-03-29 12:51:46 +0800

    add album support

--- src/api/index.js
+++ src/api/index.js
@@ -204,7 +204,7 @@ const chooseUploadImage = () =>
         wx.chooseImage({
             count: 1, // 默认9
             sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
             success(res) {
                 const localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                 wx.uploadImage({
