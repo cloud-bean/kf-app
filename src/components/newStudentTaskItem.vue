ebd0500e92e4ce5e4e29fb2b37461c0b25bdf869
xinnix <xinnix2000@gmail.com>
2018-05-17 14:10:22 +0800

    new student task add

--- src/components/newStudentTaskItem.vue
+++ src/components/newStudentTaskItem.vue
@@ -1,21 +1,31 @@
 <template>
 <!--v-bind:class="{mask:expire}"-->
-  <div class="task-item Grid" v-bind:class="[taskdata.isDone?'done':'']">
+  <!-- <div class="task-item Grid" v-bind:class="[taskdata.isDone?'done':'']" v-if="!taskdata.isDone">
+    <img :src="taskdata.titleImage.URL" alt="" style="height:7rem;position:absolute;left:0;top:0;z-index:-1;">
     <div class="task-content Cell -10of12">
       <div class=" ">
         <div class="title">{{taskdata.name}}</div>
       </div>
     </div>
-    <!-- <div class="task-right Cell -1of12">
+     <div class="task-right Cell -1of12">
       <span style="color:green;" v-if="taskdata.isDone"><i class="fa fa-check-circle"></i></span>
     </div> -->
     <!-- <div class="mask" v-if="expire">
-    </div> -->
     </div>
-
-
-
-
+    </div>
+  -->
+    <div class="news-item Grid  -center" :style="{backgroundImage: 'url(' + taskdata.titleImage.URL+'?imageView2/2/w/400/h/200/q/75|imageslim' + ')'}">
+        <div class="news-content Cell -9of12">
+          {{taskdata.name}}
+          <!-- <div class="subtitle">
+            {{newsData.subTitle}}
+          </div> -->
+        <div class="" v-if="taskdata.isDone">
+          <mt-badge class="" size="normal" style="margin-left:.3rem;" color="#6F2DBD">已完成</mt-badge>
+        </div>
+        </div>
+        <!-- <img v-bind:src="newsData.file.URL+'?imageView2/2/w/200/h/100/q/75|imageslim'" alt=""/> -->
+    </div>
 </template>
 
 <script>
@@ -48,67 +58,31 @@ const moment = require('moment');
 
 
 <style scoped>
-p{
-  margin-bottom: 2px;
-}
-.mask {
-  top: 0;
-  left: 0;
-  height:100%;
-  width:100%;
-  text-align: center;
-  vertical-align: middle;
+.news-item {
+  padding: 1.5rem 2rem;
+  font-size: 1.2rem;
+  border-radius:5px;
   color: #fff;
-  /* background: rgba(255, 255, 255, 0.8); */
-  /*z-index: -1;*/
-  position: absolute;
-  background-color: rgba(0,0,0,.5);
-  /* border: 2px solid #ccc; */
-  border-left:none;
-  border-radius: 2px;
-
-
+  border:3px solid #6F2DBD;
+  background-size: cover;
 }
-
-.no-margin{
-  margin: 0;
-  margin-top: 0.3rem;
-  border-radius: 0;
-}
-.task-item{
-  /* margin-top: 5px; */
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  border-radius: 5px;
-  /*border: 3px solid #4FC1E9;*/
-  /* border-left:none; */
-  background-color: rgba(52, 177, 220, 0.12);
-  box-shadow:0 0 1px rgba(52, 177, 220, 0.4);
-   /*padding: 0.5rem 0;*/
-  margin: 5px;
-}
-.task-content{
-    /* padding: 0 1rem; */
-    /* background-color: #e8f0f6; */
-    margin: .5rem .5rem;
-
-}
-.title{
-  padding:0.3rem 0;
-  font-size: 1rem;
-  color: #333;
-}
-
-.task-footer{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: baseline;
-  padding: 0.5rem 1rem;
-  /* background-color: #e8f0f6; */
+/* .subtitle{
+  margin-top: .5rem;
   font-size: 0.8rem;
+  color: #ccc;
+
+} */
+.news-content{
+  background-color: rgba(0,0,0,.4);
+  padding: 1rem .2rem;
+  text-align: center;
 }
+/* .date{
+  font-size: 0.8rem;
+  color: #ccc;
+}
+img{
+  width: 100%;
+} */
 
 </style>
